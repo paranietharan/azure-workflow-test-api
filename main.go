a8eec69711dce57fe08d1e029c17fa55225c2694
Paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-08-06 14:51:07 +0530

    Test endpoints added

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	//
+	// Initialize Gin router
+	r := gin.Default()
+	r.GET("/test", TestEndPoint)
+	r.GET("/", RootEndPoint)
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
+}
+
+func RootEndPoint(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message":  "Hello World",
+		"endPoint": "/",
+	})
+}
+
+func TestEndPoint(c *gin.Context) {
+	c.JSON(
+		200,
+		gin.H{
+			"Message":  "Hello World - TestEndPoint",
+			"endPoint": "/test",
+		},
+	)
 }
